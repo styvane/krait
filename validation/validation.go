652203a4bdbd042f3b1c8ac5b0f783f911168c8a
styvane <4968379+styvane@users.noreply.github.com>
2020-03-22 20:13:48 +0100

    Switch to different phonenumber package

--- validation/validation.go
+++ validation/validation.go
@@ -4,18 +4,18 @@ import (
 	"errors"
 	"strings"
 
-	"github.com/nyaruka/phonenumbers"
+	"github.com/ttacon/libphonenumber"
 )
 
 // PhoneNumber validate the phone number
 func PhoneNumber(phoneNumber, countryCode string) error {
-	p, err := phonenumbers.Parse(phoneNumber, countryCode)
+	p, err := libphonenumber.Parse(phoneNumber, countryCode)
 	if err != nil {
 		return err
 	}
 	c := int(p.GetCountryCode())
 	countryCode = strings.ToUpper(countryCode)
-	if !phonenumbers.IsValidNumber(p) || phonenumbers.GetRegionCodeForCountryCode(c) != countryCode {
+	if !libphonenumber.IsValidNumber(p) || libphonenumber.GetRegionCodeForCountryCode(c) != countryCode {
 		return errors.New("Invalid phone number")
 	}
 	return nil
