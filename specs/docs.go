3c372c0a0575082bd441d0950c63b0f76458ba00
styvane <4968379+styvane@users.noreply.github.com>
2020-03-22 20:14:50 +0100

    Add swagger specs

---
+++ specs/docs.go
@@ -0,0 +1,18 @@
+/* Package classification Krait API
+
+Krait API Documentation
+
+Schemes: http, https
+BasePath: /v1
+Version: 0.0.0
+Contact: Styvane Soukossi<mstyvane@gmail.com>
+
+Consumes:
+- application/json
+
+Produces:
+- application/json
+
+swagger:meta
+*/
+package apiserver
